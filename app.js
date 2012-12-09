3adf67b4236d8549c9ee80328fef60aa7b689aaf
Duncan Angus Wilkie (mrdnk) <duncanwilkie@hotmail.com>
2012-12-09 10:30:35 +0000

    Added process.env.PORT

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public'));
 });
 
-app.configure('development', function(){
+app.configure('development', function () {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
-app.configure('production', function(){
+app.configure('production', function () {
   app.use(express.errorHandler());
 });
 
@@ -32,5 +32,6 @@ app.configure('production', function(){
 app.get('/', routes.index);
 app.get('/services', routes.services);
 
-app.listen(3000);
+var port = process.env.PORT || 5000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
